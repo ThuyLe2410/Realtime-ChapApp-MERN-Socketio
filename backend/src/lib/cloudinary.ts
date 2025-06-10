7a545ebaf3d7f5d92d73b6e1f7a074a0ddf84d0f
Thuy Le <ngocthuyle19@gmail.com>
2025-06-10 12:40:52 -0400

    cloudinary

--- backend/src/lib/cloudinary.ts
+++ backend/src/lib/cloudinary.ts
@@ -1,12 +1,24 @@
-import {v2 as cloudinary} from "cloudinary"
-import { config } from "dotenv"
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
 
-config()
+config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// text connection
+export const testCloudinaryConnection = async () => {
+  try {
+    const result = await cloudinary.api.ping();
+    console.log("Cloudinary connected successfully", result);
+    return true;
+  } catch (error) {
+    console.log("Cloudinary connection failed");
+    return false;
+  } 
+};
 
 export default cloudinary;

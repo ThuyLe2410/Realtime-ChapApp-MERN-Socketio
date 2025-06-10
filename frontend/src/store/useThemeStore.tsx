16a97089bf3b57ee57b26d7e6b1877ee5f28b4ff
Thuy Le <ngocthuyle19@gmail.com>
2025-06-10 12:37:22 -0400

    use theme

--- frontend/src/store/useThemeStore.tsx
+++ frontend/src/store/useThemeStore.tsx
@@ -2,7 +2,7 @@ import {create} from "zustand";
 
 export const useThemeStore = create((set) => ({
     theme: localStorage.getItem("chat-theme") || "coffee",
-    setTheme:(theme) => {
+    setTheme:(theme: string) => {
         localStorage.setItem("chat-theme", theme);
         set({theme})
     }

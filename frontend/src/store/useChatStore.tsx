a42b940ee91edffe8287a5209989e109b1290d1c
Thuy Le <ngocthuyle19@gmail.com>
2025-06-10 12:39:27 -0400

    useChat

---
+++ frontend/src/store/useChatStore.tsx
@@ -0,0 +1,56 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+import { AxiosError } from "axios";
+import type { messagesProps, sendMessageDataProps, userProps } from "../types";
+type ErrorResponse = {
+  message: string;
+};
+
+export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      const err = error as AxiosError<ErrorResponse>;
+      toast.error(err.response?.data.message || "get users failed");
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId:string) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      const err = error as AxiosError<ErrorResponse>;
+      toast.error(err.response?.data.message || "get messages failed");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  setSelectedUser: (selectedUser: userProps| null) => set({selectedUser}),
+
+  sendMessage: async(data: sendMessageDataProps) => {
+    const {selectedUser, messages} = get() as {selectedUser:userProps, messages: messagesProps[]};
+    try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, data);
+        set({messages: [...messages, res.data]})   
+    } catch(error) {
+        const err = error as AxiosError<ErrorResponse>;
+      toast.error(err.response?.data.message || "send messages failed");
+    }
+  }
+
+}));

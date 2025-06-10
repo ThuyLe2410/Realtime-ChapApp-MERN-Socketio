8de80917c280da9839c91b23072c85ee820c4e55
Thuy Le <ngocthuyle19@gmail.com>
2025-06-10 12:37:49 -0400

    add message routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import { testCloudinaryConnection } from "./lib/cloudinary";
 
 import authRoutes from "./routes/auth.route";
 import messageRoutes from "./routes/message.route";
@@ -10,12 +12,18 @@ dotenv.config()
 const app = express()
 const PORT=process.env.PORT
 
-app.use(express.json())
+app.use(express.json({limit:'50mb'}))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}))
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
     connectDB()
+    testCloudinaryConnection()
 })

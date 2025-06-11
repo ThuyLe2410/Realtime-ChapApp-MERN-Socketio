5f48ed3ab6a87565d6da7c091381d37f9dfe1d7a
Thuy Le <ngocthuyle19@gmail.com>
2025-06-11 10:58:26 -0400

    AuthImage Pattern

--- frontend/src/components/AuthImagePattern.tsx
+++ frontend/src/components/AuthImagePattern.tsx
@@ -1,6 +1,6 @@
 
 
-export default function AuthImagePattern({ title, subtitle }) {
+export default function AuthImagePattern({ title, subtitle } : {title:string, subtitle:string}) {
   return (
     <div className="lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">

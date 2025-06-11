ab5415535b0e23d847686fd1852384c6022b2b50
Thuy Le <ngocthuyle19@gmail.com>
2025-06-11 14:57:16 -0400

    message socketio

--- frontend/src/store/useChatStore.tsx
+++ frontend/src/store/useChatStore.tsx
@@ -2,12 +2,14 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 import { AxiosError } from "axios";
-import type { messagesProps, sendMessageDataProps, userProps } from "../types";
+import type { ChatState, sendMessageDataProps, userProps } from "../types";
+import { useAuthStore } from "./useAuthStore";
 type ErrorResponse = {
   message: string;
 };
 
-export const useChatStore = create((set, get) => ({
+
+export const useChatStore = create<ChatState>((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -43,14 +45,31 @@ export const useChatStore = create((set, get) => ({
   setSelectedUser: (selectedUser: userProps| null) => set({selectedUser}),
 
   sendMessage: async(data: sendMessageDataProps) => {
-    const {selectedUser, messages} = get() as {selectedUser:userProps, messages: messagesProps[]};
+    const {selectedUser, messages} = get();
     try {
-        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, data);
+        const res = await axiosInstance.post(`/messages/send/${selectedUser?._id}`, data);
         set({messages: [...messages, res.data]})   
     } catch(error) {
         const err = error as AxiosError<ErrorResponse>;
       toast.error(err.response?.data.message || "send messages failed");
     }
+  },
+
+  subscribeToMessages: () => {
+    const {selectedUser} = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+    socket?.on("newMessage", (newMessage) => {
+        set({messages:[...get().messages, newMessage]});
+        
+    })
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket
+    socket?.off("newMessage")
   }
+
 
 }));
